767192ccba2d7e13a8fd56acc0294eb393e18221
Arne Gevaert <arnegevaert@gmail.com>
2018-02-07 17:57:59 +0100

    First (buggy) version

--- main.js
+++ main.js
@@ -30,44 +30,55 @@ class CSA {
     }
 
     shiftVector(vector) {
-        // TODO
         // Shift vector to the right
         // Insert Infinity on left side
+        let result = [Infinity];
+        for (let i = 0; i < vector.length - 1; i++) {
+            result.push(vector[i]);
+        }
+        return result;
     }
 
     minVector(vectors) {
-        // TODO
         // Calculate component-wise minimum of vectors (array)
+        let result = [];
+        for (let i = 0; i < vectors[0].length; i++) {
+            let components = [];
+            vectors.forEach(v => components.push(v[i]));
+            result.push(Math.min(...components));
+        }
+        return result;
     }
+
 
     // Returns profile function (for each amount of legs)
     // starting from given departure stop and departure time
     evalProfile(profile, depTime, depStop, maxLegs) {
-        let i = 0;
-        while (i < profile[depStop].length) {
-            if (profile[depStop].depTime >= depTime) {
-                return profile[depStop].arrTimes;
+        let i = profile[depStop].length - 1;
+        while (i >= 0) {
+            if (profile[depStop][i].depTime > depTime) {
+                return profile[depStop][i].arrTimes;
             }
-            i++;
+            i--;
         }
-        return [Infinity].repeat(maxLegs);
+        return Array(maxLegs).fill(Infinity);
     }
 
     calculateProfile(target, maxLegs) {
-        // profile contains profile function, objects sorted by departure time for each stop!
+        // profile contains profile function, objects sorted by descending departure time for each stop!
         let profile = {}; // {depStop: [{depTime: dt, arrTimes: [arrTime]}]}
         this.stops.forEach(stop => {
-            profile[stop] = [];
+            profile[stop] = [{depTime: Infinity, arrTimes: Array(maxLegs).fill(Infinity)}];
         });
         let tripTimes = {};
-        this.trips.forEach(t => {tripTimes[t.id] = [Infinity].repeat(maxLegs)});
+        this.trips.forEach(t => {tripTimes[t.id] = Array(maxLegs).fill(Infinity)});
         this.sortedConnections.forEach(connection => {
             // Calculate time for getting off here and walking to the target
             let x = Infinity;
             if (connection.arr.stop === target) {
                 x = connection.arr.time + this.getWalkingDistance(target, target);
             }
-            let walkTime = [x].repeat(this.maxLegs);
+            let walkTime = Array(maxLegs).fill(x);
 
             // Calculate time for remaining seated
             let remainTime = tripTimes[connection.tripId];
@@ -83,14 +94,15 @@ class CSA {
             let earliestArrival = profile[connection.dep.stop][0].arrTimes;
             let minVector = this.minVector([connectionMinTimes, earliestArrival]);
             if (minVector !== earliestArrival) {
-                // TODO Add (c dep time − (c dep stop ) change , minVector) at the front of S[c dep stop ]
+                let changeTime = this.getWalkingDistance(connection.dep.stop, connection.dep.stop);
+                let depTime = connection.dep.time - changeTime;
+                profile[connection.dep.stop].push({depTime: depTime, arrTimes: minVector});
             }
             tripTimes[connection.tripId] = connectionMinTimes;
-        })
+        });
+        return profile;
     }
-
-
 }
 
 let csa = new CSA('test.json', 10);
-csa.calculateProfile("t");
+console.log(JSON.stringify(csa.calculateProfile("t", 5)));
