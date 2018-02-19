042e1788ca4a8dd9444008a9b2f87f3b0aa5429d
Arne Gevaert <arnegevaert@gmail.com>
2018-02-19 15:10:46 +0100

    Moving to AsyncIterator interface

---
+++ main.js
@@ -0,0 +1,17 @@
+const WalkingDistanceCalculator = require('./lib/WalkingDistanceCalculator');
+const JourneyExtractor = require('./lib/JourneyExtractor');
+const JSONConnectionProvider = require('./lib/JSONConnectionProvider');
+const CSA = require('./lib/CSA');
+
+let wdc = new WalkingDistanceCalculator('data/test3.json');
+let cp = new JSONConnectionProvider('data/test3.json');
+
+let csa = new CSA(cp, wdc);
+let je = new JourneyExtractor(csa);
+
+
+csa.calculateProfile("a5", 5).then(profile => {
+    je.extractJourneys(profile, "a1", "a5", 0).then(journeys => {
+        console.log(JSON.stringify(journeys, null, 4));
+    });
+});
