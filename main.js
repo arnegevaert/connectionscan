658d3a94d0d3c9c966b25366de954dd86e953123
Arne Gevaert <arnegevaert@gmail.com>
2018-03-05 11:57:13 +0100

    IRailConnectionProvider progress but not yet working

--- main.js
+++ main.js
@@ -1,18 +1,6 @@
-const JSONConnectionProvider = require('./lib/JSONConnectionProvider');
 const InfinityISDCalculator = require('./lib/InfinityISDCalculator');
+const IRailConnectionProvider = require('./lib/IRailConnectionProvider');
 const CSA = require('./lib/CSA');
-
-/*let jsonCP = new JSONConnectionProvider('data/lc_test_data.jsonld');
-let infinityISDC = new InfinityISDCalculator();
-let csa = new CSA(jsonCP, infinityISDC, false);
-
-let stop1 = "http://irail.be/stations/NMBS/008811221";
-let stop2 = "http://irail.be/stations/NMBS/008811254";
-let depTime = new Date("2018-02-08T08:50:00.000Z");
-
-csa.getJourneys(stop1, stop2, depTime, 5).then(journeys => {
-    console.log(JSON.stringify(journeys, null, 4));
-});*/
 
 let stop1 = "http://irail.be/stations/NMBS/008811221";
 let stop2 = "http://irail.be/stations/NMBS/008811254";
@@ -20,3 +8,9 @@ let depTimeBounds = {
     lower: new Date("2018-03-01T08:00:00Z"),
     upper: new Date("2018-03-01T12:00:00Z")
 };
+let irailCP = new IRailConnectionProvider("https://graph.irail.be/sncb/connections");
+let infinityISDC = new InfinityISDCalculator();
+let csa = new CSA(irailCP, infinityISDC, false);
+csa.getJourneys(stop1, stop2, depTimeBounds, 5).then(journeys => {
+    console.log(JSON.stringify(journeys, null, 4));
+});
