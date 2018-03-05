b436cd5491ec19602ada030d0d51afca222928ba
Arne Gevaert <arnegevaert@gmail.com>
2018-03-05 15:34:18 +0100

    Still buggy with transfers

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ const InfinityISDCalculator = require('./lib/InfinityISDCalculator');
 const IRailConnectionProvider = require('./lib/IRailConnectionProvider');
 const CSA = require('./lib/CSA');
 
-let stop1 = "http://irail.be/stations/NMBS/008811221";
-let stop2 = "http://irail.be/stations/NMBS/008811254";
+let stop1 = "http://irail.be/stations/NMBS/008896008"; // Kortrijk
+let stop2 = "http://irail.be/stations/NMBS/008812005"; // Brussel-Noord
 let depTimeBounds = {
     lower: new Date("2018-03-01T08:00:00Z"),
     upper: new Date("2018-03-01T12:00:00Z")
