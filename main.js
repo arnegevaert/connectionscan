cf37b448e45d8c6fe7a5b11e6793c7c7b1ab751f
Arne Gevaert <arnegevaert@gmail.com>
2018-03-05 16:58:58 +0100

    Some bugfixes. Closes #14

--- main.js
+++ main.js
@@ -6,10 +6,10 @@ let stop1 = "http://irail.be/stations/NMBS/008896008"; // Kortrijk
 let stop2 = "http://irail.be/stations/NMBS/008812005"; // Brussel-Noord
 let depTimeBounds = {
     lower: new Date("2018-03-01T08:00:00Z"),
-    upper: new Date("2018-03-01T12:00:00Z")
+    upper: new Date("2018-03-01T10:00:00Z")
 };
 let irailCP = new IRailConnectionProvider("https://graph.irail.be/sncb/connections");
-let infinityISDC = new InfinityISDCalculator();
+let infinityISDC = new InfinityISDCalculator(60*1000); // 1 minute transfer time
 let csa = new CSA(irailCP, infinityISDC, false);
 csa.getJourneys(stop1, stop2, depTimeBounds, 5).then(journeys => {
     console.log(JSON.stringify(journeys, null, 4));
