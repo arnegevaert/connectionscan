1155857b0069d6f288d426c3023eecedebf3f550
Arne Gevaert <arnegevaert@gmail.com>
2018-03-19 15:11:56 +0100

    Pass ldfetch + don't calculate lower bound. Closes #20

--- main.js
+++ main.js
@@ -2,13 +2,17 @@ const InfinityISDCalculator = require("./lib/ISDCalculators/InfinityISDCalculato
 const LCConnectionProvider = require("./lib/ConnectionProviders/LinkedConnections/LCConnectionProvider");
 const EarliestArrivalHeuristic = require("./lib/BoundsCalculators/EarliestArrivalHeuristic");
 const CSA = require("./lib/CSA");
+const ldfetch = require("ldfetch");
+
 
 let stop1 = "http://irail.be/stations/NMBS/008896008"; // Kortrijk
 let stop2 = "http://irail.be/stations/NMBS/008812005"; // Brussel-Noord
 
-let irailCP = new LCConnectionProvider("https://graph.irail.be/sncb/connections");
+let ldf = new ldfetch();
+
+let irailCP = new LCConnectionProvider("https://graph.irail.be/sncb/connections", ldf);
 let infinityISDC = new InfinityISDCalculator(60*1000); // 1 minute transfer time
-let boundsCalculator = new EarliestArrivalHeuristic("https://graph.irail.be/sncb/connections", infinityISDC);
+let boundsCalculator = new EarliestArrivalHeuristic("https://graph.irail.be/sncb/connections", infinityISDC, ldf);
 let csa = new CSA(irailCP, infinityISDC, boundsCalculator, false);
 csa.getJourneys(stop1, stop2, new Date("2018-03-01T08:00:00Z"), 5).then(journeys => {
     console.log(JSON.stringify(journeys, null, 4));
