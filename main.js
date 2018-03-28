42bc29421c748615f5e872f340d64d9394c4109e
Pieter Colpaert <pieter.colpaert@ugent.be>
2018-03-28 15:07:27 +0200

    Added HTTP response times to stderr

--- main.js
+++ main.js
@@ -10,6 +10,22 @@ let stop2 = "http://irail.be/stations/NMBS/008812005"; // Brussel-Noord
 
 let ldf = new ldfetch();
 
+//Show HTTP Requests and response times
+var httpStartTimes = {};
+var httpResponseTimes = {};
+ldf.on('request', function (url) {
+  httpStartTimes[url] = new Date();
+});
+
+ldf.on('redirect', function (obj) {
+  httpStartTimes[obj.to] = httpStartTimes[obj.from];
+});
+
+ldf.on('response', function (url) {
+  httpResponseTimes[url] = (new Date()).getTime() - httpStartTimes[url].getTime();
+  console.error('HTTP GET - ' + url + ' (' + httpResponseTimes[url] + 'ms)');
+});
+
 let irailCP = new LCConnectionProvider("https://graph.irail.be/sncb/connections", ldf);
 let infinityISDC = new InfinityISDCalculator(60*1000); // 1 minute transfer time
 let boundsCalculator = new EarliestArrivalHeuristic("https://graph.irail.be/sncb/connections", infinityISDC, ldf);
